f59b2c2f86bb57146d4d0aaf0cdf55ab6d5e62ad
Philip <pcappelli901@gmail.com>
2019-04-29 21:50:24 -0400

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'never-the-less' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for never-the-less
+  pod 'Firebase/Core'
+
+end
---
+++ never-the-less/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  never-the-less
+//
+//  Created by Philip on 4/29/19.
+//  Copyright © 2019 Philip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
